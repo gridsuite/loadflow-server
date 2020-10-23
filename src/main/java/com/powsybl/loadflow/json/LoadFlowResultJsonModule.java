8d1562d2ea614b6746f712aa2894b1a81a5baebc
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-10-23 15:16:29 +0200

    Remove duplicated code
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/main/java/com/powsybl/loadflow/json/LoadFlowResultJsonModule.java
+++ src/main/java/com/powsybl/loadflow/json/LoadFlowResultJsonModule.java
@@ -1,10 +1,10 @@
 /**
- * Copyright (c) 2018, All partners of the iTesla project (http://www.itesla-project.eu/consortium)
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package org.gridsuite.loadflow.server.json;
+package com.powsybl.loadflow.json;
 
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.powsybl.loadflow.LoadFlowResult;
