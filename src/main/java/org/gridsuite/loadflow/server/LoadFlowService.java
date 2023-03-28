b303de833927e770febb5ba5b22498a55bdf2aba
David BRAQUART <david.braquart@rte-france.com>
2023-03-28 15:03:05 +0200

    update to use gridsuite.services

--- src/main/java/org/gridsuite/loadflow/server/LoadFlowService.java
+++ src/main/java/org/gridsuite/loadflow/server/LoadFlowService.java
@@ -50,7 +50,7 @@ import static org.gridsuite.loadflow.server.LoadFlowConstants.REPORT_API_VERSION
 @Service
 class LoadFlowService {
 
-    @Value("${report-server.base-uri:http://report-server}")
+    @Value("${gridsuite.services.report-server.base-uri:http://report-server}")
     String reportServerURI;
 
     @Value("${loadflow.default-provider}")
