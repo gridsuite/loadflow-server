0a0af638a87ae59f67de8a19a6086a901cf84abd
etiennehomer <etiennehomer@gmail.com>
2023-04-24 16:56:24 +0200

    Upgrade to gridsuite-dependencies 23 (#54)
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/main/java/org/gridsuite/loadflow/server/LoadFlowService.java
+++ src/main/java/org/gridsuite/loadflow/server/LoadFlowService.java
@@ -12,6 +12,8 @@ import com.powsybl.commons.PowsyblException;
 import com.powsybl.commons.reporter.Reporter;
 import com.powsybl.commons.reporter.ReporterModel;
 import com.powsybl.computation.local.LocalComputationManager;
+import com.powsybl.dynaflow.DynaFlowConstants;
+import com.powsybl.dynaflow.DynaFlowParameters;
 import com.powsybl.iidm.mergingview.MergingView;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.VariantManagerConstants;
@@ -73,6 +75,11 @@ class LoadFlowService {
         LoadFlowParameters params = parameters != null ? parameters : new LoadFlowParameters();
         LoadFlowResult result;
         String providerToUse = provider != null ? provider : defaultProvider;
+
+        // TODO: to be removed after loads merge fix in powsybl-dynaflow
+        if (providerToUse.equals(DynaFlowConstants.DYNAFLOW_NAME)) {
+            params.addExtension(DynaFlowParameters.class, new DynaFlowParameters().setMergeLoads(false));
+        }
 
         Reporter rootReporter = Reporter.NO_OP;
         Reporter reporter = Reporter.NO_OP;
