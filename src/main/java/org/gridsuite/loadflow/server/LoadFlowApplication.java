f9c1f6bee3760a697b1c02264ebd54e050e15a75
Sylvain Bouzols <sylvain.bouzols@gmail.com>
2023-11-23 10:01:02 +0100

    Update to gridsuite dependencies 28 (#76)
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/main/java/org/gridsuite/loadflow/server/LoadFlowApplication.java
+++ src/main/java/org/gridsuite/loadflow/server/LoadFlowApplication.java
@@ -7,7 +7,6 @@
 package org.gridsuite.loadflow.server;
 
 import com.powsybl.network.store.client.NetworkStoreService;
-import com.powsybl.ws.commons.Utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,7 +20,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class LoadFlowApplication {
 
     public static void main(String[] args) {
-        Utils.initProperties();
         SpringApplication.run(LoadFlowApplication.class, args);
     }
 }
