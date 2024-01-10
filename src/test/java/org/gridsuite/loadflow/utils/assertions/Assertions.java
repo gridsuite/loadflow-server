d08049db51b0eafe0195a61126380dd1ac6423b8
Ayoub LABIDI <ayoub.labidi@protonmail.com>
2024-01-10 16:35:14 +0100

    Requested changes
    Signed-off-by: Ayoub LABIDI <ayoub.labidi@protonmail.com>

--- src/test/java/org/gridsuite/loadflow/utils/assertions/Assertions.java
+++ src/test/java/org/gridsuite/loadflow/utils/assertions/Assertions.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -10,7 +10,7 @@ import org.assertj.core.util.CheckReturnValue;
 import org.gridsuite.loadflow.server.dto.parameters.LoadFlowParametersInfos;
 
 /**
- *  @author Tristan Chuine <tristan.chuine at rte-france.com>
+ *  @author Ayoub LABIDI <ayoub.labidi at rte-france.com>
  * {@link org.assertj.core.api.Assertions Assertions} completed with our custom assertions classes.
  */
 public class Assertions extends org.assertj.core.api.Assertions {
--- src/test/java/org/gridsuite/loadflow/utils/assertions/DTOAssert.java
+++ src/test/java/org/gridsuite/loadflow/utils/assertions/DTOAssert.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -14,8 +14,7 @@ import java.util.Date;
 import java.util.UUID;
 
 /**
- *  @author Tristan Chuine <tristan.chuine at rte-france.com>
- *  @author Slimane Amar <slimane.amar at rte-france.com>
+ *  @author Ayoub LABIDI <ayoub.labidi at rte-france.com>
  */
 public class DTOAssert<T> extends AbstractAssert<DTOAssert<T>, T> {
     public DTOAssert(T actual) {
