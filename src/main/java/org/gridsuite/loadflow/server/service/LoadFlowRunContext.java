84593eaa7299f753f15b557b5f67b70a8a04a64e
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2024-06-21 17:02:41 +0200

    Fix update of specific parameters

--- src/main/java/org/gridsuite/loadflow/server/service/LoadFlowRunContext.java
+++ src/main/java/org/gridsuite/loadflow/server/service/LoadFlowRunContext.java
@@ -7,6 +7,7 @@
 package org.gridsuite.loadflow.server.service;
 
 import com.powsybl.commons.PowsyblException;
+import com.powsybl.commons.config.PlatformConfig;
 import com.powsybl.commons.extensions.Extension;
 import com.powsybl.loadflow.LoadFlowParameters;
 import com.powsybl.loadflow.LoadFlowProvider;
@@ -35,8 +36,9 @@ public class LoadFlowRunContext extends AbstractComputationRunContext<LoadFlowPa
         LoadFlowProvider lfProvider = LoadFlowProvider.findAll().stream()
                 .filter(p -> p.getName().equals(provider))
                 .findFirst().orElseThrow(() -> new PowsyblException("LoadFLow provider not found " + provider));
-        Extension<LoadFlowParameters> extension = lfProvider.loadSpecificParameters(parameters.specificParameters())
+        Extension<LoadFlowParameters> extension = lfProvider.loadSpecificParameters(PlatformConfig.defaultConfig())
                 .orElseThrow(() -> new PowsyblException("Cannot add specific loadflow parameters with provider " + provider));
+        lfProvider.updateSpecificParameters(extension, parameters.specificParameters());
         params.addExtension((Class) extension.getClass(), extension);
         return params;
     }
