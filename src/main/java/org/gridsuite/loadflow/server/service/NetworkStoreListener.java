f29fe29f4b6ce66e13ea8288c4a0b3741a11be4a
Slimane AMAR <amarsli@gm0winl104.bureau.si.interne>
2025-04-24 16:58:29 +0200

    Review
    Signed-off-by: Slimane AMAR <amarsli@gm0winl104.bureau.si.interne>

--- src/main/java/org/gridsuite/loadflow/server/service/NetworkStoreListener.java
+++ src/main/java/org/gridsuite/loadflow/server/service/NetworkStoreListener.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class NetworkStoreListener implements NetworkListener {
     private static final Logger LOG = LoggerFactory.getLogger(NetworkStoreListener.class);
@@ -16,10 +17,12 @@ public class NetworkStoreListener implements NetworkListener {
 
     private record Modification(Identifiable<?> identifiable, String attribute, Object oldValue, Object newValue) { }
 
+    private Set flowAttributes = Set.of("p1", "p2", "q1", "q2");
+
     @Override
     public void onCreation(Identifiable<?> identifiable) {
         modificationList.add(new Modification(identifiable, "CREATE", null, null));
-        LOG.error("NetworkStoreListener onCreation".concat(identifiable.getId()));
+        LOG.error("NetworkStoreListener onCreation {}", identifiable.getId());
     }
 
     @Override
@@ -35,7 +38,9 @@ public class NetworkStoreListener implements NetworkListener {
     @Override
     public void onUpdate(Identifiable<?> identifiable, String attribute, String variantId, Object oldValue, Object newValue) {
         modificationList.add(new Modification(identifiable, attribute, oldValue, newValue));
-        LOG.error("NetworkStoreListener onUpdate".concat(identifiable.getId()));
+        if (!flowAttributes.contains(attribute)) {
+            LOG.error("NetworkStoreListener onUpdate {} : {}", identifiable.getType(), attribute);
+        }
     }
 
     @Override
@@ -61,12 +66,12 @@ public class NetworkStoreListener implements NetworkListener {
     @Override
     public void onPropertyAdded(Identifiable<?> identifiable, String key, Object newValue) {
         modificationList.add(new Modification(identifiable, key, null, newValue));
-        LOG.error("NetworkStoreListener onPropertyAdded".concat(identifiable.getId()));
+        LOG.error("NetworkStoreListener onPropertyAdded {} : {}", identifiable.getId(), key);
     }
 
     @Override
     public void onPropertyReplaced(Identifiable<?> identifiable, String key, Object oldValue, Object newValue) {
-        LOG.error("NetworkStoreListener onPropertyReplaced".concat(identifiable.getId()));
+        LOG.error("NetworkStoreListener onPropertyReplaced {} : {}", identifiable.getId(), key);
     }
 
     @Override
