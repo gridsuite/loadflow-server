d1918091482c1719450504f61159e0585082d2c7
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2025-05-15 08:44:00 +0200

    feat: transformerVoltageControlOn is now passed as controller param instead of stored in database
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/main/java/org/gridsuite/loadflow/server/entities/parameters/LoadFlowParametersEntity.java
+++ src/main/java/org/gridsuite/loadflow/server/entities/parameters/LoadFlowParametersEntity.java
@@ -48,9 +48,6 @@ public class LoadFlowParametersEntity {
     @Enumerated(EnumType.STRING)
     private LoadFlowParameters.VoltageInitMode voltageInitMode;
 
-    @Column(name = "transformerVoltageControlOn", columnDefinition = "boolean default false", nullable = false)
-    private boolean transformerVoltageControlOn = false;
-
     @Column(name = "useReactiveLimits", columnDefinition = "boolean default true", nullable = false)
     private boolean useReactiveLimits = true;
 
@@ -165,7 +162,6 @@ public class LoadFlowParametersEntity {
 
     private void assignCommonValues(LoadFlowParameters allCommonValues) {
         voltageInitMode = allCommonValues.getVoltageInitMode();
-        transformerVoltageControlOn = allCommonValues.isTransformerVoltageControlOn();
         useReactiveLimits = allCommonValues.isUseReactiveLimits();
         phaseShifterRegulationOn = allCommonValues.isPhaseShifterRegulationOn();
         twtSplitShuntAdmittance = allCommonValues.isTwtSplitShuntAdmittance();
@@ -196,7 +192,6 @@ public class LoadFlowParametersEntity {
     public LoadFlowParameters toLoadFlowParameters() {
         return LoadFlowParameters.load()
                 .setVoltageInitMode(this.getVoltageInitMode())
-                .setTransformerVoltageControlOn(this.isTransformerVoltageControlOn())
                 .setUseReactiveLimits(this.isUseReactiveLimits())
                 .setPhaseShifterRegulationOn(this.isPhaseShifterRegulationOn())
                 .setTwtSplitShuntAdmittance(this.isTwtSplitShuntAdmittance())
