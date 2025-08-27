80fc507cb785d310f69961171a9efb3148724c57
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2025-08-27 14:01:56 +0200

    fix: header + author
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/test/java/org/gridsuite/loadflow/server/LoadFlowWorkerServiceTest.java
+++ src/test/java/org/gridsuite/loadflow/server/LoadFlowWorkerServiceTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.loadflow.server;
 
 import com.powsybl.iidm.network.Network;
@@ -11,6 +17,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * @author Kevin Le Saulnier <kevin.le-saulnier at rte-france.com>
+ */
 @SpringBootTest
 class LoadFlowWorkerServiceTest {
     @Test
