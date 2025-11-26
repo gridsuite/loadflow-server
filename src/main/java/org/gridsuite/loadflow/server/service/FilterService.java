100c636d538f7720e290604fc8c5a7d67658fc74
basseche <bassel.el-cheikh_externe@rte-france.com>
2025-11-26 17:47:48 +0100

    change filterService to work with new filter library

--- src/main/java/org/gridsuite/loadflow/server/service/FilterService.java
+++ src/main/java/org/gridsuite/loadflow/server/service/FilterService.java
@@ -15,6 +15,7 @@ import org.gridsuite.computation.service.AbstractFilterService;
 import org.gridsuite.filter.utils.EquipmentType;
 import org.gridsuite.loadflow.server.dto.Column;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,8 +31,8 @@ public class FilterService extends AbstractFilterService {
 
     public FilterService(
             NetworkStoreService networkStoreService,
-            @Value("${gridsuite.services.filter-server.base-uri:http://filter-server/}") String filterServerBaseUri) {
-        super(networkStoreService, filterServerBaseUri);
+            @Value("${gridsuite.services.filter-server.base-uri:http://filter-server/}") String filterServerBaseUri, RestTemplateBuilder restTemplateBuilder) {
+        super(restTemplateBuilder, networkStoreService, filterServerBaseUri);
     }
 
     public Optional<ResourceFilterDTO> getResourceFilter(@NonNull UUID networkUuid, @NonNull String variantId, @NonNull GlobalFilter globalFilter) {
